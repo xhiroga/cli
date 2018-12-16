2cd73e3f19b0ce56fe9dd1a0f743a25e5cfa98c0
小笠原寛明 <hiroga@hirogas-Mac-mini.local>
2018-12-16 11:01:47 +0900

    integrate to 1 file

---
+++ r2z.sh
@@ -0,0 +1,55 @@
+# 指定されたパスが.rarなら解凍してディレクトリにする。ディレクトリなら処理を続行。ただのファイルなら何もしない。
+# その後ディレクトリの中身に対して、再帰的に処理を実行する。
+# 完了したら指定されたパスorディレクトリをzipファイルにする
+set -e
+
+function check_dir() {
+    # ここは""で囲わなくても代入できる
+    dir=$1
+    echo "[dir: ${dir}]"
+    if [ ! -d "${dir}" ]; then
+        echo "[dir not found]"
+        exit 1
+    fi
+    echo "[find \"${dir}\" -mindepth 1 -maxdepth 1]"
+    # -Eオプションがないと-regexオプションは動かないぽい
+    # -print0 オプションを-regexよりも前に持ってくると機能しなくなる
+    find -E "${dir}" -mindepth 1 -maxdepth 1 -regex .*\.rar -type f -print0 | xargs -0 -n1 -P1 bash ./r2z.sh
+    find "${dir}" -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -n1 -P1 bash ./r2z.sh
+}
+
+function rar2zip(){
+    # 指定した.rarファイルと同じ階層に.rarファイルと同じ名前でフォルダを作成し、そこにファイルを解凍します。
+    rar=$1
+    echo "[rar: ${rar}]"
+    if [ ! -e "${rar}" ]; then
+        echo "[rar not found]"
+        exit 1
+    fi
+
+    dir=${rar%%\.rar}
+    mkdir "${dir}"
+    echo "[dir: ${dir}]"
+
+    unrar x "${rar}" "${dir}"
+    rm "${rar}"
+
+    check_dir "${dir}"
+
+    zip -jr "${dir}.zip" "${dir}" -x .DS_Store
+    rm -rf "${dir}"
+}
+
+path=$1
+echo "[path: ${path}]"
+
+if [ ! -e "${path}" ]; then
+    echo "[path not found]"
+    exit 1
+elif  [ -d "${path}" ]; then
+    echo "[path: ${path} = dir]"
+    check_dir "${path}"
+elif [ "${path##*.}" = "rar" ]; then
+    echo "[path: ${path} = .rar]"
+    rar2zip "${path}"
+fi
