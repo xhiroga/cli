4a1321ff345a4c028f14fbbe55b9881c5d830d56
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-01-30 20:20:43 +0900

    put scripts to ./local/bin

--- init.sh
+++ init.sh
@@ -1,1 +1,2 @@
 find * -type f -exec chmod +x {} \;
+find * -type f ! -name ".git" ! -path "./.git/*" ! -name ".gitignore" ! -name "init.sh" ! -name "README.md" | xargs -I {} ln -f {} ~/.local/bin/{}
