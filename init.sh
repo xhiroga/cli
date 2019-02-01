bcbb2206a2c625d67abc166662828be16b28b740
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-02-01 23:50:34 +0900

    add dir

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
+mkdir -p ~/.local/bin
 find * -type f -exec chmod +x {} \;
 find * -type f ! -name ".git" ! -path "./.git/*" ! -name ".gitignore" ! -name "init.sh" ! -name "README.md" | xargs -I {} ln -f {} ~/.local/bin/{}
